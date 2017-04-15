03b370e9858f01ccb423d798f284f494b9b4131c
puneet kala <puneet@docsapp.in>
2017-04-16 01:25:40 +0530

    removed unused stuff

--- app/src/main/java/com/kalap/contacts/ContactFragmentPagerAdapter.java
+++ app/src/main/java/com/kalap/contacts/ContactFragmentPagerAdapter.java
@@ -1,7 +1,6 @@
 package com.kalap.contacts;
 
 import android.net.Uri;
-import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
