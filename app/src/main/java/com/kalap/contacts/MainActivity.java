ba3aa78fb548473891a852383fefbca1e1ba8cbd
puneet kala <puneet@docsapp.in>
2017-04-16 17:43:29 +0530

    putting contact loading to async task

--- app/src/main/java/com/kalap/contacts/MainActivity.java
+++ app/src/main/java/com/kalap/contacts/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.os.AsyncTask;
 import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
@@ -34,43 +35,8 @@ public class MainActivity extends AppCompatActivity {
         switch (requestCode) {
             case MY_READ_CONTACT_PERMISSION_REQUEST: {
                 if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    HashMap<String, Contact> contactHashMap = new HashMap<>();
-                    ContentResolver contentResolver = getContentResolver();
-                    Cursor cursor1 = contentResolver.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, null, null, "upper(" + ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME + ") ASC");
-                    if (cursor1 != null) {
-                        while (cursor1.moveToNext()) {
-                            Contact contact;
-                            String name = cursor1.getString(cursor1.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
-                            String phoneNum = cursor1.getString(cursor1.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
-                            ArrayList<String> phNumList;
-                            if (contactHashMap.containsKey(name)) {
-                                contact = contactHashMap.get(name);
-                                phNumList = contact.getPhoneNumberList();
-                                phNumList.add(phoneNum);
-                                contact.setPhoneNumberList(phNumList);
-                                contactHashMap.put(name,contact);
-                            } else {
-                                contact = new Contact();
-                                contact.setName(name);
-                                phNumList = new ArrayList<>();
-                                phNumList.add(phoneNum);
-                                contact.setPhoneNumberList(phNumList);
-                                contactHashMap.put(name,contact);
-                            }
-                        }
-                        cursor1.close();
-                    }
-                    for (String name : contactHashMap.keySet()) {
-                        ContactsDatabaseHelper helper = new ContactsDatabaseHelper(getApplicationContext());
-                        helper.addContact(contactHashMap.get(name));
-                    }
-                    SharedPreferences sharedPreferences1 = getSharedPreferences("contactsPreferences", MODE_PRIVATE);
-                    SharedPreferences.Editor editor = sharedPreferences1.edit();
-                    editor.putLong("lastFetchTime", System.currentTimeMillis());
-                    editor.apply();
-                    loadingContacts.setVisibility(View.INVISIBLE);
-                    startActivity(new Intent(MainActivity.this,ContactActivity.class));
-                    finish();
+                    ContactLoader contactLoader = new ContactLoader();
+                    contactLoader.execute();
                 }
             }
         }
@@ -98,42 +64,8 @@ public class MainActivity extends AppCompatActivity {
                 if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
                     ActivityCompat.requestPermissions(MainActivity.this,new String[] {Manifest.permission.READ_CONTACTS},MY_READ_CONTACT_PERMISSION_REQUEST);
                 } else {
-                    HashMap<String, Contact> contactHashMap = new HashMap<>();
-                    ContentResolver contentResolver = getContentResolver();
-                    Cursor cursor1 = contentResolver.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, null, null, "upper(" + ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME + ") ASC");
-                    if (cursor1 != null) {
-                        while (cursor1.moveToNext()) {
-                            Contact contact;
-                            String name = cursor1.getString(cursor1.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
-                            String phoneNum = cursor1.getString(cursor1.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
-                            ArrayList<String> phNumList;
-                            if (contactHashMap.containsKey(name)) {
-                                contact = contactHashMap.get(name);
-                                phNumList = contact.getPhoneNumberList();
-                                phNumList.add(phoneNum);
-                                contact.setPhoneNumberList(phNumList);
-                            } else {
-                                contact = new Contact();
-                                contact.setName(name);
-                                phNumList = new ArrayList<>();
-                                phNumList.add(phoneNum);
-                                contact.setPhoneNumberList(phNumList);
-                            }
-                            contactHashMap.put(name,contact);
-                        }
-                        cursor1.close();
-                    }
-                    for (String name : contactHashMap.keySet()) {
-                        ContactsDatabaseHelper helper = new ContactsDatabaseHelper(getApplicationContext());
-                        helper.addContact(contactHashMap.get(name));
-                    }
-                    SharedPreferences sharedPreferences1 = getSharedPreferences("contactsPreferences", MODE_PRIVATE);
-                    SharedPreferences.Editor editor = sharedPreferences1.edit();
-                    editor.putLong("lastFetchTime", System.currentTimeMillis());
-                    editor.apply();
-                    loadingContacts.setVisibility(View.INVISIBLE);
-                    startActivity(new Intent(MainActivity.this,ContactActivity.class));
-                    finish();
+                    ContactLoader contactLoader = new ContactLoader();
+                    contactLoader.execute();
                 }
             } else {
                 preparingApp.setText("Loading...");
@@ -143,6 +75,56 @@ public class MainActivity extends AppCompatActivity {
             }
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    private class ContactLoader extends AsyncTask<String,String,String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            HashMap<String, Contact> contactHashMap = new HashMap<>();
+            ContentResolver contentResolver = getContentResolver();
+            Cursor cursor1 = contentResolver.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, null, null, "upper(" + ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME + ") ASC");
+            if (cursor1 != null) {
+                while (cursor1.moveToNext()) {
+                    Contact contact;
+                    String name = cursor1.getString(cursor1.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+                    String phoneNum = cursor1.getString(cursor1.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                    ArrayList<String> phNumList;
+                    if (contactHashMap.containsKey(name)) {
+                        contact = contactHashMap.get(name);
+                        phNumList = contact.getPhoneNumberList();
+                        phNumList.add(phoneNum);
+                        contact.setPhoneNumberList(phNumList);
+                        contactHashMap.put(name,contact);
+                    } else {
+                        contact = new Contact();
+                        contact.setName(name);
+                        phNumList = new ArrayList<>();
+                        phNumList.add(phoneNum);
+                        contact.setPhoneNumberList(phNumList);
+                        contactHashMap.put(name,contact);
+                    }
+                }
+                cursor1.close();
+            }
+            for (String name : contactHashMap.keySet()) {
+                ContactsDatabaseHelper helper = new ContactsDatabaseHelper(getApplicationContext());
+                helper.addContact(contactHashMap.get(name));
+            }
+            SharedPreferences sharedPreferences1 = getSharedPreferences("contactsPreferences", MODE_PRIVATE);
+            SharedPreferences.Editor editor = sharedPreferences1.edit();
+            editor.putLong("lastFetchTime", System.currentTimeMillis());
+            editor.apply();
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            loadingContacts.setVisibility(View.INVISIBLE);
+            startActivity(new Intent(MainActivity.this,ContactActivity.class));
+            finish();
         }
     }
 }
