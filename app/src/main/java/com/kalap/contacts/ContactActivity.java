8f7d5434f776316c6312294e4d7bd8dd738ff021
puneet kala <puneet@docsapp.in>
2017-08-27 02:07:23 +0530

    switching

--- app/src/main/java/com/kalap/contacts/ContactActivity.java
+++ app/src/main/java/com/kalap/contacts/ContactActivity.java
@@ -5,42 +5,72 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.TabLayout;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
-import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
+import android.view.MenuItem;
 
-import com.kalap.contacts.adapters.ContactFragmentPagerAdapter;
-
-public class ContactActivity extends AppCompatActivity implements View.OnClickListener {
+public class ContactActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
     private static final int CALL_PHONE_REQUEST = 102;
-    private ContactFragmentPagerAdapter adapter;
+    private Uri data;
 
     @Override
     public void onCreate(Bundle savedInstance) {
         super.onCreate(savedInstance);
-        Uri data = getIntent().getData();
+        data = getIntent().getData();
         setContentView(R.layout.activity_contact);
-        if(ContextCompat.checkSelfPermission(ContactActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(ContactActivity.this,new String[] {Manifest.permission.CALL_PHONE},CALL_PHONE_REQUEST);
+        if (ContextCompat.checkSelfPermission(ContactActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(ContactActivity.this, new String[]{Manifest.permission.CALL_PHONE}, CALL_PHONE_REQUEST);
         }
-        ViewPager viewPager = (ViewPager) findViewById(R.id.view_pager);
-        adapter = new ContactFragmentPagerAdapter(getSupportFragmentManager(), data);
-        viewPager.setAdapter(adapter);
-
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
-        tabLayout.setupWithViewPager(viewPager);
+        BottomNavigationView navigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        navigationView.setOnNavigationItemSelectedListener(this);
+        changeFragment(0);
     }
 
-    @Override
-    public void onClick(View v) {
+    private void shareApp() {
         String text = "Download this simple contacts app from https://play.google.com/store/apps/details?id=com.kalap.contacts";
         Intent intent = new Intent(Intent.ACTION_SEND);
         intent.setType("text/plain");
-        intent.putExtra(Intent.EXTRA_TEXT,text);
-        startActivity(Intent.createChooser(intent,"Share via"));
+        intent.putExtra(Intent.EXTRA_TEXT, text);
+        startActivity(Intent.createChooser(intent, "Share via"));
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_dial: {
+                changeFragment(0);
+                return true;
+            }
+            case R.id.action_recent: {
+                changeFragment(1);
+                return true;
+            }
+            case R.id.action_all: {
+                changeFragment(2);
+                return true;
+            }
+            case R.id.action_share: {
+                shareApp();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void changeFragment(int position) {
+        Fragment newFragment;
+        if (position == 0) {
+            newFragment = DialerFragment.newInstance(data);
+        } else if (position == 1) {
+            newFragment = CallLogsFragment.newInstance();
+        } else {
+            newFragment = ContactListFragment.newInstance();
+        }
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, newFragment).commit();
     }
 }
