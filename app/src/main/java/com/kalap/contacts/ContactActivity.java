1f8d635e7b741656ec9a384a78e3371b6c934159
puneet kala <puneet@docsapp.in>
2017-08-27 01:39:17 +0530

    the

--- app/src/main/java/com/kalap/contacts/ContactActivity.java
+++ app/src/main/java/com/kalap/contacts/ContactActivity.java
@@ -1,29 +1,38 @@
 package com.kalap.contacts;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatImageView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import com.kalap.contacts.adapters.ContactFragmentPagerAdapter;
 
-public class ContactActivity extends AppCompatActivity {
+public class ContactActivity extends AppCompatActivity implements View.OnClickListener {
 
     public static final String TAG = "ContactActivity";
     private static final int CALL_PHONE_REQUEST = 102;
     private ContactFragmentPagerAdapter adapter;
+    private Toolbar toolbar;
+    private AppCompatImageView share;
 
     @Override
     public void onCreate(Bundle savedInstance) {
         super.onCreate(savedInstance);
         Uri data = getIntent().getData();
         setContentView(R.layout.activity_contact);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        share = (AppCompatImageView) findViewById(R.id.share);
+        share.setOnClickListener(this);
         if(ContextCompat.checkSelfPermission(ContactActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(ContactActivity.this,new String[] {Manifest.permission.CALL_PHONE},CALL_PHONE_REQUEST);
         }
@@ -33,5 +42,14 @@ public class ContactActivity extends AppCompatActivity {
 
         TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
         tabLayout.setupWithViewPager(viewPager);
+    }
+
+    @Override
+    public void onClick(View v) {
+        String text = "Download this simple contacts app from https://play.google.com/store/apps/details?id=com.kalap.contacts";
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT,text);
+        startActivity(Intent.createChooser(intent,"Share via"));
     }
 }
