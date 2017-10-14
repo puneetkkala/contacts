3c8682057458005d054f175c422221f9c4f9e5f4
puneet kala <puneet@docsapp.in>
2017-10-14 16:43:29 +0530

    contact object migrated to kotlin

---
+++ app/src/main/java/com/kalap/contacts/object/Contact.kt
@@ -0,0 +1,9 @@
+package com.kalap.contacts.`object`
+
+import java.util.*
+
+class Contact {
+    var id: String = ""
+    var name: String = ""
+    var phoneNumberList: ArrayList<String> = ArrayList()
+}
